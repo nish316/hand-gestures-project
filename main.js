512c6a1bfdf55d244a6f918e621a5fd741def1cb
Nishka <82759534+nish316@users.noreply.github.com>
2021-12-17 17:32:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -28,7 +28,50 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function speak(){
   var synth = window.speechSynthesis;
   speak_data_1 = "The first prediction is " + prediction_1;
-  speak_data_2 = "And the second prediction is " + prediction_2;
   var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
   synth.speak(utterThis);
+}
+
+function check()
+{
+  img = document.getElementById('captured_image');
+  classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, results) 
+{
+if (error) {
+  console.error(error);
+
+} else {
+  console.log(results);
+  document.getElementById("result_emotion_name").innerHTML = results[0].label;
+  prediction_1 = results[0].label;
+  speak();
+  if(results[0].label == "ok")
+  {
+    document.getElementById("update_emoji").innerHTML = "&#128076;";
+  }
+  if(results[0].label == "victory")
+  {
+    document.getElementById("update_emoji").innerHTML = "&#9996;";
+  }
+  if(results[0].label == "thumbs")
+  {
+    document.getElementById("update_emoji").innerHTML = "&#128077;";
+  }
+  if(results[0].label == "rock")
+  {
+    document.getElementById("update_emoji").innerHTML = "&#129304;";
+  }
+  if(results[0].label == "fist")
+  {
+    document.getElementById("update_emoji").innerHTML = "&#9994;";
+  }
+  if(results[0].label == "pointer")
+  {
+    document.getElementById("update_emoji").innerHTML = "&#128070;";
+  }
+}
 }
